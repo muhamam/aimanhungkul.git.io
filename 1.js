2d94fe2bda43a5c3b43a73a2b14a1b68fff5f2f6
aimanhungkul <muhammadanasaiman0@gmail.com207006101@student.unsil.ac.idgit initgit add .git commit -m first>
2021-05-03 10:00:52 +0800

    ganti semua file yang ada

--- 1.js
+++
@@ -1,8 +0,0 @@
-const saklar = document.querySelector('.saklar');
-const nav = document.querySelector('nav ul');
-
-saklar.addEventListener('click', function () {
-    nav.classList.toggle('slide')
-}
-
-)
