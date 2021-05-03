1b200d97a33070899c59758f072066f899ea75fb
aimanhungkul <muhammadanasaiman0@gmail.com207006101@student.unsil.ac.idgit initgit add .git commit -m first>
2021-05-03 09:46:49 +0800

    first commit

---
+++ 1.js
@@ -0,0 +1,8 @@
+const saklar = document.querySelector('.saklar');
+const nav = document.querySelector('nav ul');
+
+saklar.addEventListener('click', function () {
+    nav.classList.toggle('slide')
+}
+
+)
